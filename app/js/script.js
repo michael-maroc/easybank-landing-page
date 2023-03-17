8652fd94f3cc98da2a1fd4b7ed61d1707acac747
Michael <maroc.michael@hotmail.com>
2023-03-17 17:40:16 +0100

    ajout de l'animation du menu hamburger du header

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,18 @@
 const btnHamburger = document.getElementById("btn-hamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay")
 
 btnHamburger.addEventListener("click", function() {
   console.log('open hamburger');
-  if (btnHamburger.classList.contains('open')) {
-    btnHamburger.classList.remove('open')
-  } else {
-    btnHamburger.classList.add('open');
+
+  if (header.classList.contains('open')) { // Close hamburger menu
+    header.classList.remove('open')
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
+  }
+  else {  // Open hamburger menu
+    header.classList.add('open');
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');
   }
 });

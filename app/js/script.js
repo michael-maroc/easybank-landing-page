d7c76f9bc557eedaffcde80bb526cb6ea084fc91
Michael <maroc.michael@hotmail.com>
2023-03-22 15:04:58 +0100

    ajout de l'animation pour le menu mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,29 @@
 const btnHamburger = document.getElementById("btn-hamburger");
+const body = document.querySelector('body');
 const header = document.querySelector(".header");
-const overlay = document.querySelector(".overlay")
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener("click", function() {
   console.log('open hamburger');
 
   if (header.classList.contains('open')) { // Close hamburger menu
+    body.classList.remove('noscroll');
     header.classList.remove('open')
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
     overlay.classList.remove('fade-in');
     overlay.classList.add('fade-out');
   }
   else {  // Open hamburger menu
+    body.classList.add('noscroll');
     header.classList.add('open');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
     overlay.classList.remove('fade-out');
     overlay.classList.add('fade-in');
   }
